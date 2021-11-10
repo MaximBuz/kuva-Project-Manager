0dc1f43322d1d9014f76f0556bd90435c753b638
Maxim Buz <mbuz.maxim@gmail.com>
2021-11-10 15:47:48 +0100

    create user document on new sign in

--- frontend/src/redux/user/user.actions.js
+++ frontend/src/redux/user/user.actions.js
@@ -1,6 +1,7 @@
 import userTypes from './user.types';
-import { auth, provider, handleUserProfile } from '../../firebase-config';
-import { signInWithPopup } from "firebase/auth"
+import { db, auth, provider, handleUserProfile } from '../../firebase-config';
+import { signInWithPopup } from "firebase/auth";
+import { collection, addDoc} from "firebase/firestore"; 
 
 export const setCurrentUser = user => ({
   type: userTypes.SET_CURRENT_USER,
@@ -11,7 +12,17 @@ export const setCurrentUser = user => ({
 export const signInWithGoogle = () => async (dispatch) => {
   try {
     await signInWithPopup(auth, provider)
-      .then(() => {
+      .then(async (result) => {
+        if (result.additionalUserInfo.isNewUser) {
+          let user = result.user;
+          const docRef = await addDoc(collection(db, "users"), {
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoUrl,
+            id: user.uid,
+            jobTitle: "No-Job-Title"
+        });
+        }
         dispatch({
           type: userTypes.SIGN_IN_SUCCESS,
           payload: true
