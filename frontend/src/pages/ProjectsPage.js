c09326d5e7d3ef2021c3b1bb9fac4eab09dc1e02
Maxim Buz <mbuz.maxim@gmail.com>
2021-12-16 14:10:33 +0100

    added archived project view

--- frontend/src/pages/ProjectsPage.js
+++ frontend/src/pages/ProjectsPage.js
@@ -16,11 +16,14 @@ import styled from "styled-components";
 import moment from "moment";
 
 function ProjectsPage() {
+  // Handling archived / unarchived state
+  const [archived, setArchived] = useState(false);
+
   // Handling open and close of new project modal
   const [openModal, setOpenModal] = useState(false);
 
   // Reading current state from Redux
-  const { projects } = useSelector((state) => state.projects);
+  const { projects, archivedProjects } = useSelector((state) => state.projects);
   const { currentUser } = useSelector((state) => state.user);
 
   // If logged in, get projects from firestore
@@ -32,9 +35,14 @@ function ProjectsPage() {
   // Count Projects
   let projectCount = projects.length;
   useEffect(() => {
-    console.log(projects.length);
     projectCount = projects.length;
   }, [projects]);
+
+  // Count Archived Projects
+  let archivedProjectCount = archivedProjects.length;
+  useEffect(() => {
+    archivedProjectCount = archivedProjects.length;
+  }, [archivedProjects]);
 
   return (
     <Wrapper>
@@ -48,32 +56,67 @@ function ProjectsPage() {
         </p>
       </WelcomeWrapper>
       <ProjectCounter>
-        <h2>your projects</h2>
-        <CounterBlob count={projectCount || 0} />
+        {archived ? (
+          <>
+            <h2>your archived projects</h2>
+            <CounterBlob count={archivedProjectCount || 0} />
+          </>
+        ) : (
+          <>
+            <h2>your active projects</h2>
+            <CounterBlob count={projectCount || 0} />
+          </>
+        )}
       </ProjectCounter>
+      <SwitchWrapper>
+        Active
+        <Switch class="switch" onClick={() => setArchived(!archived)}>
+          <input type="checkbox" checked={archived} />
+          <Slider></Slider>
+        </Switch>
+        Archived
+      </SwitchWrapper>
       <ProjectsWrapper>
-        {projects &&
-          projects.map((item, index) => {
-            return (
-              <ProjectCard
-                index={index}
-                id={item.id}
-                projectKey={item.projectKey}
-                projectTitle={item.projectTitle}
-                timeStamp={moment(new Date(item.timeStamp.seconds * 1000))
-                  .fromNow()
-                  .toString()}
-                projectSummary={item.projectSummary}
-              />
-            );
-          })}
-        <CreateNewCard
-          onClick={() => {
-            setOpenModal(!openModal);
-          }}
-        >
-          <UilPlusCircle className="add-icon" size="50" color="#51515170" />
-        </CreateNewCard>
+        {archived
+          ? archivedProjects &&
+            archivedProjects.map((item, index) => {
+              return (
+                <ProjectCard
+                  index={index}
+                  id={item.id}
+                  projectKey={item.projectKey}
+                  projectTitle={item.projectTitle}
+                  timeStamp={moment(new Date(item.timeStamp.seconds * 1000))
+                    .fromNow()
+                    .toString()}
+                  projectSummary={item.projectSummary}
+                />
+              );
+            })
+          : projects &&
+            projects.map((item, index) => {
+              return (
+                <ProjectCard
+                  index={index}
+                  id={item.id}
+                  projectKey={item.projectKey}
+                  projectTitle={item.projectTitle}
+                  timeStamp={moment(new Date(item.timeStamp.seconds * 1000))
+                    .fromNow()
+                    .toString()}
+                  projectSummary={item.projectSummary}
+                />
+              );
+            })}
+        {!archived && (
+          <CreateNewCard
+            onClick={() => {
+              setOpenModal(!openModal);
+            }}
+          >
+            <UilPlusCircle className="add-icon" size="50" color="#51515170" />
+          </CreateNewCard>
+        )}
       </ProjectsWrapper>
 
       {openModal && <Modal closeModal={setOpenModal} />}
@@ -89,6 +132,14 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 40px;
+`;
+
+const SwitchWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+  margin-top: -30px;
 `;
 
 const WelcomeWrapper = styled.div`
@@ -113,6 +164,65 @@ const ProjectCounter = styled.div`
     color: #35307e;
     font-size: larger;
     font-weight: 500;
+  }
+`;
+
+const Slider = styled.span``;
+
+const Switch = styled.div`
+  position: relative;
+  display: inline-block;
+  width: 40px;
+  height: 26px;
+
+  input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+
+  ${Slider} {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+
+    :before {
+      position: absolute;
+      content: "";
+      height: 20px;
+      width: 20px;
+      left: 4px;
+      bottom: 3px;
+      background-color: white;
+      -webkit-transition: 0.4s;
+      transition: 0.4s;
+    }
+  }
+
+  input:checked + ${Slider} {
+    background-color: #2196f3;
+  }
+
+  input:focus + ${Slider} {
+    box-shadow: 0 0 1px #2196f3;
+  }
+
+  input:checked + ${Slider}:before {
+    transform: translateX(13px);
+  }
+
+  ${Slider} {
+    border-radius: 34px;
+  }
+
+  ${Slider}:before {
+    border-radius: 50%;
   }
 `;
 
